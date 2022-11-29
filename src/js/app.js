290d41d9aaae6749008d33d2d996f99bacca54ed
PMLujan <lujan_paredes@hotmail.com.ar>
2022-11-29 16:32:25 -0300

    dark mode a paginas internas y preferencia en sistema operativo

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,19 @@ document.addEventListener('DOMContentLoaded', function (){
 });
 
 function darkmode(){
+   const prefiereModeDark=window.matchMedia('(prefers-color-scheme: dark)');//si tiene activado modo oscuro en el sistema operativo 
+          if(prefiereModeDark.matches){
+            document.body.classList.add('dark-mode');
+          }else{
+            document.body.classList.remove('dark-mode');
+         }
+   prefiereModeDark.addEventListener('change',function(){// cambia automaticamente sgun sistema operativo sin actualizar pagina
+         if(prefiereModeDark.matches){
+            document.body.classList.add('dark-mode');
+          }else{
+            document.body.classList.remove('dark-mode');
+         }
+   })
    const darkBoton= document.querySelector('.dark-mode-boton');
          darkBoton.addEventListener('click', function(){
          document.body.classList.toggle('dark-mode');
